30ddd7a37c159eb86c47731522ac9ea0fdffc67c
francesc <fran.altes@gmail.com>
2020-11-05 13:44:00 +0000

    feat: local env

--- next-js/config/config.js
+++ next-js/config/config.js
@@ -1,5 +1,4 @@
 export default {
   serverApiUrl: "http://sinatra:9292",
-  clientApiUrl: "http://3.91.218.39:9292",
-  // clientApiUrl: "http://localhost:9292",
+  clientApiUrl: "http://localhost:9292",
 }
